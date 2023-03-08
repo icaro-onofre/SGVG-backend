11d286dfac6cc61fb0579ac4988360fe3509606c
Icaro <icaro.onofre.s@gmail>
2023-03-08 14:33:25 -0300

    Adicionando enpoints com busca utlizando prisma

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv'
 import express from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 const app = express()
+
 const port = process.env.PORT || 3000
 
 dotenv.config()
@@ -9,8 +14,22 @@ app.use(express.json())
 
 const PORT = 3000;
 
-app.listen(PORT,()=>console.log("Servidor rodando na porta"+PORT))
+app.listen(PORT,()=>console.log("Servidor rodando na porta "+PORT))
 
 app.get('/',(req,res) => {
 		res.send('Hello world');
+})
+
+app.get('/vagas', async (req,res) => {
+		const response = await prisma.vaga.findMany()
+		res.json(response)
+})
+app.get('/funcionario', async (req,res) => {
+		const response = await prisma.funcionario.findMany()
+		res.json(response)
+})
+
+app.get('/cliente', async (req,res) => {
+		const response = await prisma.cliente.findMany()
+		res.json(response)
 })
