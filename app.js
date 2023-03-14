18f6664ee5eeb6733e184d8bc26c819d4442b898
fforelle <xdollboyx@gmail.com>
2023-03-14 10:50:48 -0300

    Consertando endpoint de funcionario

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
-import dotenv from 'dotenv'
-import express from 'express';
-import clienteRouter from './routes/cliente.js';
+import dotenv from "dotenv";
+import express from "express";
+import clienteRouter from "./routes/cliente.js";
+import funcionarioRouter from "./routes/funcionario.js";
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-dotenv.config()
+dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
 const PORT = 3000;
 
-app.listen(PORT,()=>console.log("Servidor rodando na porta "+PORT))
+app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
 
 app.use(clienteRouter);
+app.use(funcionarioRouter);
 
-app.get('/',(req,res) => {
-		res.send('Servidor rodando');
-})
+app.get("/", (req, res) => {
+  res.send("Servidor rodando");
+});
 
-app.get('/signup',(req,res) => {
-		res.send('Rota de registro');
-})
+app.get("/signup", (req, res) => {
+  res.send("Rota de registro");
+});
 
-app.get('/signin',(req,res) => {
-		res.send('Rota de login');
-})
-
+app.get("/signin", (req, res) => {
+  res.send("Rota de login");
+});
